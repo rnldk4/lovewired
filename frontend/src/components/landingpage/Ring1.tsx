a988cb2504626e91e5b6ee7a47c2f28ef98e190a
ronald <cleansernld@gmail.com>
2024-10-26 19:56:17 +0200

    fix ring width

--- frontend/src/components/landingpage/Ring1.tsx
+++ frontend/src/components/landingpage/Ring1.tsx
@@ -2,10 +2,10 @@ import ring1 from "../../assets/ring1.svg";
 
 export default function Ring1() {
     return (
-        <div className="absolute overflow-hidden bottom-0 left-0 -z-10">
+        <div className="w-full absolute overflow-hidden bottom-0 left-0 -z-10">
             <img
                 src={ring1}
-                className="relative -bottom-[19rem] left-[30rem]"
+                className="relative -bottom-[19rem] left-[32rem]"
             />
         </div>
     );
