ac0cf804b8847dbe0456ba94651111426e92b5b9
ronald <cleansernld@gmail.com>
2024-10-27 03:21:33 +0100

    add hover animations

--- frontend/src/components/landingpage/Header.tsx
+++ frontend/src/components/landingpage/Header.tsx
@@ -5,13 +5,17 @@ import LoginModal from "./LoginModal";
 const Header: React.FC = () => {
     const [modalOpened, setModalOpened] = useState<boolean>(false);
 
-    const handleLoginClick = () => {
+    function handleLoginClick() {
         setModalOpened(true);
-    };
+    }
 
-    const closeModal = () => {
+    function handleContactClick() {
+        //
+    }
+
+    function closeModal() {
         setModalOpened(false);
-    };
+    }
 
     return (
         <>
@@ -21,11 +25,21 @@ const Header: React.FC = () => {
                     className="rounded-full w-36 ml-24"
                     alt="Logo"
                 />
-                <div className="flex font-poppins items-center mb-8 space-x-20 text-3xl mr-24">
-                    <p>contact us</p>
-                    <p onClick={handleLoginClick} className="cursor-pointer">
-                        login
-                    </p>
+                <div className="flex font-poppins items-center mb-8 space-x-20 text-3xl mr-32 relative">
+                    <div
+                        className="relative cursor-pointer group"
+                        onClick={handleContactClick}
+                    >
+                        <p>contact us</p>
+                        <div className="absolute -left-[20px] -bottom-[10px] h-[4px] bg-[#fe85d2] transition-all duration-300 w-0 group-hover:w-[200px]"></div>
+                    </div>
+                    <div
+                        className="relative cursor-pointer group"
+                        onClick={handleLoginClick}
+                    >
+                        <p>login</p>
+                        <div className="absolute -left-[20px] -bottom-[10px] h-[4px] bg-[#2596be] transition-all duration-300 w-0 group-hover:w-[114px]"></div>
+                    </div>
                 </div>
             </div>
             <LoginModal
