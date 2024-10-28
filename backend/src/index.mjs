2d79c50c232850d0187942f0582c94108e0f0a9e
ronald <cleansernld@gmail.com>
2024-10-28 18:16:36 +0100

    add domain to CORS

--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -22,7 +22,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173", //local dev
-            "https://lovewired.vercel.app", //vercel deployment
+            "https://lovewired.net",
         ],
         credentials: true,
     })
