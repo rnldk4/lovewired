5dd3679a74cbe13b6a4b0661b6d1294adc6e7197
ronald <cleansernld@gmail.com>
2024-10-28 03:22:43 +0100

    add homepage

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
+export default function HomePage() {
+    const navigate = useNavigate();
+
+    async function handleLogout() {
+        try {
+            const response = await fetch("http://localhost:3000/logout", {
+                method: "POST",
+                credentials: "include",
+            });
+
+            const responseMessage = await response.text();
+
+            if (!response.ok) {
+                throw new Error(`Logout failed: ${responseMessage}`);
+            }
+
+            console.log(responseMessage);
+            navigate("/");
+        } catch (err) {
+            console.log(err);
+        }
+    }
+    return (
+        <>
+            <div className="ml-20 mt-20">
+                <h1 className="text-6xl">Hello world!</h1>
+                <button
+                    className="w-40 h-20 rounded-full bg-black text-white font-outfit text-2xl mt-20"
+                    onClick={handleLogout}
+                >
+                    Log out
+                </button>
+            </div>
+        </>
+    );
+}
